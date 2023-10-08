094c98092cd4a2d5da865c3e50a1fb47c7205ecd
august <1@augustor.com>
2023-10-08 18:36:21 +0800

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,11 +24,46 @@ export default {
   methods: {
     make: function() {
       let arr = [];
-      arr.push(...this.add(this.type1, 101));
-      // arr.push(...this.wholeTenAddSingle(this.type2));
-      // arr.push(...this.wholeTenAddWholeTen(this.type3));
-      console.log(arr);
+      // arr.push(...this.addAndReduce(this.type1, 101));
+      arr.push(...this.add(40, 101));
+      arr.push(...this.multiplication(40, 101, 10));
+      arr.push(...this.wholeTenMultiplication(40));
+      arr = this.shuffleSelf(arr, 100);
       this.mathArr = arr;
+    },
+    addAndReduce: function(num, limit = 21) {
+      let n = 0;
+      let arr = [];
+      let x;
+      let a, b, c;
+      let z, z1;
+      while (n < num) {
+        x = Math.floor(Math.random() * 2);
+        a = Math.floor(Math.random() * limit);
+        if (x == 0) {
+          b = Math.floor(Math.random() * (limit - a));
+          z = a + "+" + b;
+          z1 = a * 1 + b * 1;
+        } else {
+          b = Math.floor(Math.random() * a);
+          z = a + "-" + b;
+          z1 = a * 1 - b * 1;
+        }
+        if (a < 6 && b < 6) {
+          continue;
+        }
+        c = Math.floor(Math.random() * Math.floor(limit / 2));
+        if (z1 > Math.floor(limit / 2)) {
+          z += "-" + c + "=";
+        } else {
+          z += "+" + c + "=";
+        }
+        if (!arr.indexOf(z) > -1) {
+          arr.push(z);
+          n++;
+        }
+      }
+      return arr;
     },
     add: function(num, limit = 21) {
       let n = 0;
@@ -47,7 +82,7 @@ export default {
           b = Math.floor(Math.random() * a);
           z = a + "-" + b + "=";
         }
-        if (a < 6 && b < 6) {
+        if (a < 10 || b < 10) {
           continue;
         }
         if (!arr.indexOf(z) > -1) {
@@ -57,19 +92,36 @@ export default {
       }
       return arr;
     },
-    wholeTenAddSingle: function(num) {
+    multiplication: function(num, limita = 101, limitb = 10) {
+      let n = 0;
+      let arr = [];
+      let a;
+      let b;
+      let z;
+      while (n < num) {
+        a = Math.floor(Math.random() * limita);
+        b = Math.floor(Math.random() * limitb);
+        z = a + "x" + b + "=";
+        if (a <= 10 || b < 2) {
+          continue;
+        }
+        if (!arr.indexOf(z) > -1) {
+          arr.push(z);
+          n++;
+        }
+      }
+      return arr;
+    },
+    wholeTenMultiplication: function(num) {
       let n = 0;
       let x, a, b, z;
       let arr = [];
       while (n < num) {
-        x = Math.floor(Math.random() * 2);
+        x = Math.floor(Math.random() * 2) + 1;
         a = Math.floor(Math.random() * 9) + 1;
-        b = Math.floor(Math.random() * 9) + 1;
-        if (x == 0) {
-          z = a * 10 + "+" + b + "=";
-        } else {
-          z = a * 10 + "-" + b + "=";
-        }
+        a = a * Math.pow(10, x);
+        b = Math.floor(Math.random() * 6) + 4;
+        z = a + "x" + b + "=";
         if (!arr.indexOf(z) > -1) {
           arr.push(z);
           n++;
@@ -104,6 +156,25 @@ export default {
       }
 
       return arr;
+    },
+    shuffleSelf: function(array, size) {
+      let index = -1;
+      let length = array.length;
+      let lastIndex = length - 1;
+      let value;
+
+      size = size === undefined ? length : size;
+      while (++index < size) {
+        // var rand = baseRandom(index, lastIndex),
+        var rand = index + Math.floor(Math.random() * (lastIndex - index + 1));
+        value = array[rand];
+
+        array[rand] = array[index];
+
+        array[index] = value;
+      }
+      array.length = size;
+      return array;
     }
   },
   mounted() {
