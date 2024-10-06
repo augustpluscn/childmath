26a68fdb52183f1183ab061a10ab014bc2d3bcc0
august <1@augustor.com>
2024-10-06 14:29:05 +0800

    部署

--- deploy.js
+++ deploy.js
@@ -6,11 +6,11 @@ const ora = require('ora')
 const app = "childmath"
 
 const server = {
-  host: '10.0.0.230',//服务器IP
+  host: '10.0.0.240',//服务器IP
   port: 22,//服务器端口
-  username: 'august',//服务器ssh登录用户名
-  password: 'dKVKrWN8BywonQ',//服务器ssh登录密码
-  path: '/vol1/1000/nginxWebUI/wwwroot/' + app //服务器web目录
+  username: 'root',//服务器ssh登录用户名
+  password: 'Flzx-3qc',//服务器ssh登录密码
+  path: '/www/wwwroot/' + app //服务器web目录
 };
 
 const loading = ora('正在部署至【' + server.host + '】服务器')
